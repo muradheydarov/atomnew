099a039d00a39cc4884d71e5127f577a65c8fce1
Murad <muradheyderov@gmail.com>
2017-01-24 21:00:35 +0400

    functions

--- udemy.js
+++ udemy.js
@@ -1,3 +1,21 @@
-var x = 8;
-    y = 9
-alert((x + y == 1) ? 'Murad' : 'Kamran');
+// var dayofweek = 1;
+// switch (dayofweek) {
+//   case 1:
+//     alert('monday');
+//     break;
+//   case 2:
+//     alert("tuesday");
+//     break;
+//   case 3:
+//     alert("wednesday");
+//     break;
+//
+function emioglu(x, y)
+  {
+    if (x==8) {
+      return "you cant do it";
+    }
+return x + y;
+}
+var show = add(5,3);
+alert(show);
